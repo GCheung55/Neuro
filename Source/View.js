efbf3a71adfa2a03fcefc149562a3980b7f0726c
Garrick Cheung <garrick@garrickcheung.com>
2012-03-01 10:51:44 -0800

    Adding View.js. Adding Neuro.js to help build.

---
+++ Source/View.js
@@ -0,0 +1,89 @@
+require: './libs/mootools-class-extras/Source/Class.Binds';
+
+exports: View
+
+// (function(context){
+    var Unit = require('./libs/Company/Source/Company').Unit;
+
+    // creates functions to subscribe/unsubscribe based on handlers
+    var modelBinder = function(bindType){
+        return function(){
+            var prefix = this.getPrefix();
+
+            prefix && (prefix += '.');
+
+            Object.keys(this.handlers).each(function(type){
+                var obj = {},
+                    methods = Array.from(this.handlers[type]),
+                    len = methods.length,
+                    i = 0;
+
+                while(len--){
+                    obj[prefix + type] = this.bound(methods[i++]);
+                }
+
+                this[bindType](obj);
+            }, this);
+
+            return this;
+        }
+    };
+
+    var View = new Class({
+        Implements: [Class.Binds, Options, Unit],
+
+        // Model publishers / View methods mapping
+        handlers: undefined,
+
+        element: undefined,
+
+        options: {
+            handlers: {
+                'change': ['render'],
+                'destroy': 'destroy'
+            }
+        },
+
+        initialize: function(data, options){
+            this.setup(data, options);
+        },
+
+        setup: function(data, options){
+            this.setOptions(options);
+
+            this.handlers = this.options.handlers;
+
+            this.setPrefix(this.options.Prefix);
+
+            this.setupUnit();
+
+            this.bindModel();
+
+            this.render();
+
+            return this;
+        },
+
+        attachEvents: function(){ return this; },
+
+        detachEvents: function(){ return this; },
+
+        bindModel: modelBinder('subscribe'),
+
+        unbindModel: modelBinder('unsubscribe'),
+
+        render: function(){
+            this.attachEvents();
+
+            return this;
+        },
+
+        destroy: function(){
+            this.detachEvents();
+
+            this.element = (this.element.destroy(), undefined);
+
+            return this;
+        }
+    });
+// })(typeof exports != 'undefined' ? exports : window);
