9c732a7ed5f94a9fd3039cb5edfd1d1409cf08f9
Chase Wilson <chase@agroism.com>
2012-04-06 15:52:35 -0700

    Un fucking the monkey I made

--- Build/amd/Neuro.js
+++
@@ -1,6 +0,0 @@
-define(function(require, exports, module){
-exports.Observer = require('../libs/Company/Source/Company').Unit;
-exports.View = require('./View').View;
-exports.Collection = require('./Collection').Collection;
-exports.Model = require('./Model').Model;
-});
