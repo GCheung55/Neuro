ad3beffe97a9c6124311fb98c3cf85e7b6976507
Garrick Cheung <garrick@garrickcheung.com>
2012-07-23 09:51:35 -0700

    Change get/getPrevious to both receive a bool and that if a get exists but a getPrevious does not, getPrevious triggers the get method but with a true passed to it.

--- mixins/customAccessor.js
+++ mixins/customAccessor.js
@@ -1,16 +1,55 @@
+var accessTypes = ['set', 'get', 'getPrevious'],
+    getMap = {
+        get: false,
+        getPrevious: true
+    };
+
 var CustomAccessor = new Class({
+    _accessors: {},
+
     options: {
         accessors: {}
     },
 
     setupAccessors: function(){
-        this.setAccessor(this.options.accessors);
+        this.setAccessor(Object.merge({}, this._accessors, this.options.accessors));
 
         return this;
     },
 
     setAccessor: function(name, val){
-        this._accessors[name] = val;
+        var accessors = {},
+            cont = Object.keys(val).some(accessTypes.contains, accessTypes);
+
+        if (cont) {
+            /**
+             * Create a getPrevious method that is the get method,
+             * but passed a true arg to signify it should access _previousData
+             * while the get method gets passed a false value to signify it
+             * should access _data.
+             */
+            if (val.get && !val.getPrevious) {
+                accessors.getPrevious = val.get.bind(this, true);
+                accessors.get = val.get.bind(this, false);
+            }
+
+            val.set && (accessors.set = val.set.bind(this));
+
+            /**
+             * Loop through the 'get' types to define accessors functions if
+             * it doesn't already exist on the accessors object.
+             *
+             * The bool is passed to the method regardless of whether a get or
+             * getPrevious method existed for consistency.
+             */
+            Object.each(getMap, function(bool, type) {
+                if (val[type] && !accessors[type]) {
+                    accessors[type] = val[type].bind(this, bool);
+                }
+            }, this);
+
+            this._accessors[name] = accessors;
+        }
 
         return this;
     }.overloadSetter(),
