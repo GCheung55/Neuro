98a96f17f839a6c3967cbd6d2fd037b59f1b6f2a
Garrick Cheung <garrick@garrickcheung.com>
2012-07-30 17:09:34 -0700

    Removing ToDo to make room for the new one.

--- demos/ToDo/js/app.js
+++
@@ -1,7 +0,0 @@
-var loader = require('./link/Web/link')
-loader.alias('company', './company/Company')
-loader.alias('neuro', './neuro')
-loader.alias('todo', './ToDo')
-loader.base('./');
-loader.load('todo/main')
-
