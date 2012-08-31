7f7a5d8927db6db3eabe424729c582fba35aef70
Garrick Cheung <garrick@garrickcheung.com>
2012-08-31 13:04:45 -0700

    Adding patternlexer from Miller Medeiros' Crossroads.js Adding defaults, accessors, and validators for route.js

--- src/router/route.js
+++ src/router/route.js
@@ -1,4 +1,5 @@
-var Model = require('./src/model/main').Model;
+var Model = require('./src/model/main').Model,
+    patternLexer = require('./src/router/patternlexer.js');
 
 var Route = new Class({
     Extends: Model,
@@ -7,29 +8,57 @@ var Route = new Class({
         defaults: {
             // callback: undefined,
             pattern: undefined,
-            priority: 0
+            priority: 0,
+            _paramsIds: undefined,
+            _optionalParamsIds: undefined,
+            _matchRegexp: undefined
         },
         accessors: {
+            pattern: {
+                // Process the pattern before setting
+                set: function(key, value){
+                    var obj = {},
+                        lexer = this.getLexer();
+
+                    obj[key] = value;
+
+                    if (typeOf(pattern) != 'regexp') {
+                        obj._paramsIds = patternLexer.getParamIds(value);
+                        obj._optionalParamsIds = patternLexer.getOptionalParamsIds(value);
+                        obj._matchRegexp = patternLexer.compilePattern(value);
+                    } else {
+                        obj._matchRegexp = pattern;
+                    }
+
+                    this.set(obj);
+                }
+            },
             callback: {
                 set: function(key, value){
                     this.addEvent('match', value);
 
-                    this.silent(function(){
-                        this.unset(key);
-                    });
-
                     return this;
                 }
             }
+        },
+
+        validators: {
+            pattern: function(val){
+                var type = typeOf(val);
+                return type == 'regexp' || type == 'string';
+            },
+            priority: Type.isNumber
         }
     },
 
     setup: function(data, options){
         this.parent(data, options);
 
-        
-
         return this;
+    },
+
+    getLexer: function(){
+        return patternLexer;
     }
 });
 
